373185bc28f1244923d4105ae3490a011e7a3019
Elena Fernández-Llebrez <elenafernandezllebrez@gmail.com>
2024-05-13 23:35:59 +0200

    Changes to be committed: 	modified:   DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -6,9 +6,86 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
-      }
+      },
       // TODO: Include the rest of the fields of the Restaurants table
+      name:{
+        allowNull: false,
+        type: Sequelize.STRING
 
+      },
+      description:{
+        allowNull: false,
+        type: Sequelize.STRING
+
+      },
+      address:{
+        allowNull: false,
+        type: Sequelize.STRING
+
+      },
+      postalCode:{
+        allowNull: false,
+        type: Sequelize.STRING
+
+      },
+      url:{
+        allowNull: false,
+        type: Sequelize.STRING
+
+      },
+      restaurantCategoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'RestaurantCategories'
+          },
+          key: 'id'
+        }
+      },
+      shippingCosts:{
+        allowNull: false,
+        type: Sequelize.DOUBLE
+      },
+      email:{
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      logo:{
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      phone:{
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt:{
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt:{
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      userId:{
+        allowNull: false,
+
+      },
+      heroImage:{
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      status:{
+        allowNull: false,
+        type: Sequelize.ENUM,
+        values: [
+          'online',
+          'offline',
+          'closed',
+          'temporarily closed'
+        ],
+      },
     })
   },
   down: async (queryInterface, Sequelize) => {
